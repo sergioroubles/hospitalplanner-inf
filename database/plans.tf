2eabbf1c132036179c6cdb952ca68eee090e1ee0
Sergio Robles <sergioroubles@gmail.com>
2023-06-09 09:43:41 +0200

    Add plans table

---
+++ database/plans.tf
@@ -0,0 +1,21 @@
+resource "aws_dynamodb_table" "plans" {
+  name           = "plans"
+  billing_mode   = "PAY_PER_REQUEST"
+  hash_key       = "id"
+  sort_key       = "datetime"
+
+  attribute {
+    name = "id"
+    type = "S"
+  }
+
+  attribute {
+    name = "name"
+    type = "S"
+  }
+
+  attribute {
+    name = "datetime"
+    type = "S"
+  }
+}
