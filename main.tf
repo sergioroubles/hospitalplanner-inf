a9c360000aa7f61f77b7ed4302f20bfa4a2fe588
Sergio Robles <sergioroubles@gmail.com>
2023-06-09 09:04:38 +0200

    Refactor root module for provider

--- main.tf
+++ main.tf
@@ -1,15 +1,4 @@
-provider "aws" {
-    region = var.aws_region
-    access_key = var.AWS_ACCESS_KEY
-    secret_key = var.AWS_SECRET_KEY
-
-    default_tags {
-        tags = {
-            created-by = "terraform"
-        }
-    } 
-}
-
+# Modules
 module "bucket" {
     source = "./buckets"
 }
