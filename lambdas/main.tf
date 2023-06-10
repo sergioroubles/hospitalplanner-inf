84965466235d092a5b7d9f14e00298a406fdce30
Sergio Robles <sergioroubles@gmail.com>
2023-06-10 13:28:34 +0200

    Refactor lambdas module to add more lambdas

--- lambdas/main.tf
+++ lambdas/main.tf
@@ -1,12 +1,3 @@
-resource "aws_lambda_function" "api" {
-    function_name    = "lambda-api-backend"
-    s3_bucket        = var.bucket_id
-    s3_key           = "api/lambda_package.zip"
-    runtime          = "python3.9"
-    handler          = "src.main.handler"
-    role             = aws_iam_role.lambda_backend.arn
-}
-
 data "aws_iam_policy_document" "AWSLambdaTrustPolicy" {
   statement {
     actions    = ["sts:AssumeRole"]
--- main.tf
+++ main.tf
@@ -3,15 +3,15 @@ module "bucket" {
     source = "./buckets"
 }
 
-module "lambda" {
-    source = "./lambda"
+module "lambdas" {
+    source = "./lambdas"
     bucket_id = module.bucket.s3_bucket_backend_id
 }
 
 module "apigateway" {
     source = "./apigateway"
-    lambda_api_invoke_arn = module.lambda.lambda_api_invoke_arn
-    lambda_api_name = module.lambda.lambda_api_name
+    lambda_api_invoke_arn = module.lambdas.lambda_api_invoke_arn
+    lambda_api_name = module.lambdas.lambda_api_name
     bucket_id = module.bucket.s3_bucket_backend_id
     PUBLIC_SUPABASE_ANON_KEY = var.PUBLIC_SUPABASE_ANON_KEY
 }
