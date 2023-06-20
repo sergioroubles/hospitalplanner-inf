426389048fcec350966647ee52d6ea913f6e7418
Sergio Robles <sergioroubles@gmail.com>
2023-06-20 23:32:27 +0200

    Rename APIGW according to environment

--- modules/apigateway/main.tf
+++ modules/apigateway/main.tf
@@ -1,6 +1,6 @@
 resource "aws_api_gateway_rest_api" "hospitalplanner" {
-  name        = "hospitalplanner"
-  description = "Hospital Planner API Gateway"
+  name        = "hospitalplanner-${var.environment}"
+  description = "Hospital Planner API Gateway for ${var.environment} environment"
 }
 
 resource "aws_api_gateway_resource" "proxy" {
