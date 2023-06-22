d1e49866943493dffcfe157f9a7edaf81785b5f0
Sergio Robles <sergioroubles@gmail.com>
2023-06-22 22:46:07 +0200

    Add populate from api repo

---
+++ helpers/populate.py
@@ -0,0 +1,33 @@
+import json
+
+def populate(table_name: str, json_file: str) -> None:
+    dynamodb = boto3.resource('dynamodb')
+    table = dynamodb.Table(table_name)
+
+    json_data = read_json_file(file_path=json_file)
+
+    print(f"Loading '{json_file}' into '{table_name}'...")
+
+    with table.batch_writer() as batch:
+        for item in json_data:
+            batch.put_item(Item=item)
+
+    print(f"Loaded successfully.")
+    
+def read_json_file(file_path):
+    with open(file_path, 'r') as json_file:
+        data = json.load(json_file)
+    return data
+
+
+if __name__ == '__main__':
+
+    tables_files = [[f"{name}-prod", f"data/serviciozulet/{name}.json"] for name in ["restrictions", "workers", "shifts", "workers"]]
+    
+    if True:
+        import localstack_client.session as boto3
+    else:
+        import boto3
+    
+    for table_name, json_file in tables_files:
+        populate(table_name=table_name, json_file=json_file)
