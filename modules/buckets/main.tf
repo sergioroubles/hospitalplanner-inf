6704ad61531246f4b109f581f5096c8b615f1665
Sergio Robles <sergioroubles@gmail.com>
2023-06-25 19:11:52 +0200

    Modularize paths to zip lambda files

--- modules/buckets/main.tf
+++ modules/buckets/main.tf
@@ -5,39 +5,41 @@ resource "aws_s3_bucket" "backend" {
   }
 }
 
+locals {
+  lambda_zips_dir = "../../helpers/localstack/lambda_zips"
+}
 resource "aws_s3_object" "algorithm_lambda_zip" {
   count = var.environment == "local"? 1 : 0
   bucket = aws_s3_bucket.backend.bucket
   key    = "/algorithm/lambda_package.zip"
-  source = "../../environments/local/lambda_zips/algorithm/lambda_package.zip"
+  source = "${local.lambda_zips_dir}/algorithm/lambda_package.zip"
 
   # The filemd5() function is available in Terraform 0.11.12 and later
   # For Terraform 0.11.11 and earlier, use the md5() function and the file() function:
   # etag = "${md5(file("path/to/file"))}"
-  etag = filemd5("../../environments/local/lambda_zips/algorithm/lambda_package.zip")
+  etag = filemd5("${local.lambda_zips_dir}/algorithm/lambda_package.zip")
 }
 
 resource "aws_s3_object" "api_lambda_zip" {
   count = var.environment == "local"? 1 : 0
   bucket = aws_s3_bucket.backend.bucket
   key    = "/api/lambda_package.zip"
-  source = "../../environments/local/lambda_zips/api/lambda_package.zip"
+  source = "${local.lambda_zips_dir}/api/lambda_package.zip"
 
   # The filemd5() function is available in Terraform 0.11.12 and later
   # For Terraform 0.11.11 and earlier, use the md5() function and the file() function:
   # etag = "${md5(file("path/to/file"))}"
-  etag = filemd5("../../environments/local/lambda_zips/api/lambda_package.zip")
+  etag = filemd5("${local.lambda_zips_dir}/api/lambda_package.zip")
 }
-
 
 resource "aws_s3_object" "authenticator_lambda_zip" {
   count = var.environment == "local"? 1 : 0
   bucket = aws_s3_bucket.backend.bucket
   key    = "/authenticator/lambda_package.zip"
-  source = "../../environments/local/lambda_zips/authenticator/lambda_package.zip"
+  source = "${local.lambda_zips_dir}/authenticator/lambda_package.zip"
 
   # The filemd5() function is available in Terraform 0.11.12 and later
   # For Terraform 0.11.11 and earlier, use the md5() function and the file() function:
   # etag = "${md5(file("path/to/file"))}"
-  etag = filemd5("../../environments/local/lambda_zips/authenticator/lambda_package.zip")
+  etag = filemd5("${local.lambda_zips_dir}/authenticator/lambda_package.zip")
 }
