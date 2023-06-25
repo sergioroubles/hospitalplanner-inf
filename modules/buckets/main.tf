4108a0edd4de4e76c23a61b2b63299ba2d136b99
Sergio Robles <sergioroubles@gmail.com>
2023-06-25 17:08:29 +0200

    Add conditional s3 object uploads to deploy zips in localstack s3 bucket

--- modules/buckets/main.tf
+++ modules/buckets/main.tf
@@ -4,3 +4,27 @@ resource "aws_s3_bucket" "backend" {
     Name = "hospitalplanner-backend-${var.environment}"
   }
 }
+
+resource "aws_s3_object" "algorithm_lambda_zip" {
+  count = var.environment == "local"? 1 : 0
+  bucket = aws_s3_bucket.backend.bucket
+  key    = "/algorithm/lambda_package.zip"
+  source = "../../environments/local/lambda_zips/algorithm/lambda_package.zip"
+
+  # The filemd5() function is available in Terraform 0.11.12 and later
+  # For Terraform 0.11.11 and earlier, use the md5() function and the file() function:
+  # etag = "${md5(file("path/to/file"))}"
+  etag = filemd5("../../environments/local/lambda_zips/algorithm/lambda_package.zip")
+}
+
+resource "aws_s3_object" "api_lambda_zip" {
+  count = var.environment == "local"? 1 : 0
+  bucket = aws_s3_bucket.backend.bucket
+  key    = "/api/lambda_package.zip"
+  source = "../../environments/local/lambda_zips/api/lambda_package.zip"
+
+  # The filemd5() function is available in Terraform 0.11.12 and later
+  # For Terraform 0.11.11 and earlier, use the md5() function and the file() function:
+  # etag = "${md5(file("path/to/file"))}"
+  etag = filemd5("../../environments/local/lambda_zips/api/lambda_package.zip")
+}
