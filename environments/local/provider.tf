c386fd124920cb6720b983b27cfdeb9884cc433e
Sergio Robles <sergioroubles@gmail.com>
2023-06-26 20:22:13 +0200

    Adapt local provider

--- environments/local/provider.tf
+++ environments/local/provider.tf
@@ -2,6 +2,9 @@ provider "aws" {
   region     = var.aws_region
   access_key = var.AWS_ACCESS_KEY
   secret_key = var.AWS_SECRET_KEY
+  skip_credentials_validation = true
+  skip_metadata_api_check     = true
+  skip_requesting_account_id  = true
 
   default_tags {
     tags = {
@@ -9,5 +12,10 @@ provider "aws" {
       environment = var.environment
       project     = "hospitalplanner"
     }
+  }
+
+  endpoints {
+    dynamodb = "http://localhost:4566"
+    lambda   = "http://localhost:4566"
   }
 }
